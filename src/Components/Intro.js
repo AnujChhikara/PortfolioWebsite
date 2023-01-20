9cc2684dae3e38361e272ae0ed24aa7cf3493386
AnujChhikara <anujchhikara777@gmail.com>
2023-01-20 21:56:44 +0530

    techbox

--- src/Components/Intro.js
+++ src/Components/Intro.js
@@ -1,34 +1,40 @@
 import Typewriter from "typewriter-effect";
 
-
 function Intro(props) {
   return (
-    <div className='md:flex md:flex-row font-Sans sm:flex sm:flex-col md:mx-72 sm:mx-10 sm:items-center items-center space-x-2 fade-in-left' >
-    <img className='md:w-60 sm:w-32 ' src="/profile.png" alt="" />
-    <div className='text-4xl md:pl-4 md:pb-16 sm:mt-2 md:mt-0 '>
-      <h2 className=''>👋 Hi There!</h2>
-    <div className='flex space-x-2 mt-3 ' >
-    <h2 className=''> I'm </h2>
-    <h3 className='text-[#D62AD0]  md:text-3xl sm:text-2xl border border-xl border-[#D62AD0] shadow-[#D62AD0] rounded shadow-lg p-1 px-3 '>Anuj Chhikara</h3>
-    </div>
-    
-    <div className={`text-2xl mt-4 absolute ${props.darkMode ? 'text-gray-300': 'text-[#434242]'}  `}>
-      
-      <Typewriter
-        onInit={(typewriter) => {
-          typewriter.typeString("I am a self-learned MERN stack developer")
-          .pauseFor(600)
-          .deleteAll()
-          .typeString(`Forever a student of code, always learning 🚀`)
-          .start()
-        }}
-      
+    <div className="md:flex md:flex-row font-Sans sm:flex sm:flex-col md:mx-72 sm:mx-10 sm:items-center items-center space-x-2 fade-in-left">
+      <img
+        className="md:w-60 sm:w-32 rounded-full "
+        src="/profile.jpeg"
+        alt=""
       />
-      
+      <div className="md:text-4xl sm:text-xl md:pl-4 md:pb-16 sm:mt-2 md:mt-0 ">
+        <h2 className="">👋 Hi There!</h2>
+        <div className="flex space-x-2 mt-3 ">
+          <h2 className=""> I'm </h2>
+          <h3 className="text-[#D62AD0] fill md:text-3xl sm:text-2xl border border-xl border-[#D62AD0] shadow-[#D62AD0] rounded shadow-lg p-1 px-3 ">
+            Anuj Chhikara
+          </h3>
+        </div>
+
+        <div
+          className={`md:text-2xl sm:text-lg mt-4 absolute ${
+            props.darkMode ? "text-gray-300" : "text-[#434242]"
+          }  `}
+        >
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("I am a self-learned MERN stack developer")
+                .pauseFor(600)
+                .deleteAll()
+                .typeString(`Forever a student of code, always learning 🚀`)
+                .start();
+            }}
+          />
+        </div>
       </div>
     </div>
-    </div>
-    
-  )
+  );
 }
-export default Intro
+export default Intro;
--- src/Home.js
+++ src/Home.js
@@ -1,43 +1,47 @@
-import React, { useState } from 'react'
-import Snowman from "./Components/Snowman"
-import Snow from './Components/Snow';
-import './App.css'
-import "animate.css"
-import Intro from './Components/Intro';
+import React, { useState } from "react";
 
+import Snow from "./Components/Snow";
+import "./App.css";
+import "animate.css";
+import Intro from "./Components/Intro";
+import Tech from "./Components/Tech";
 
-import Navbar from './Components/Navbar'
+import Navbar from "./Components/Navbar";
 
 function Home() {
-  const[darkMode, setDarkMode] = useState(false)
-  function toggleDarkMode(){
-    setDarkMode(prevDarkMode=> ! prevDarkMode)
+  const [darkMode, setDarkMode] = useState(false);
+  function toggleDarkMode() {
+    setDarkMode((prevDarkMode) => !prevDarkMode);
   }
 
-
   return (
-    <div className={`font-nunito font-semibold min-h-screen m-w-screen ${darkMode? 'bg-gradient-to-b from-gray-700 via-gray-900 to-black text-white' :'bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-gray-900 via-gray-100 to-gray-900' } `}>
-      <Snow/>
-      
+    <div
+      className={`font-nunito flex flex-col font-semibold w-screen h-max ${
+        darkMode
+          ? "bg-gradient-to-b from-gray-700 via-gray-900 to-black text-white"
+          : "bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-gray-900 via-gray-100 to-gray-900"
+      } `}
+    >
+      <Snow />
 
-     <div className='absolute top-0'>
-      
-     <div className="md:mb-28 sm:mb-20 "><Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode}/></div>
-     </div>
-     <div className='absolute md:top-40 sm:top-32'><Intro darkMode={darkMode} /></div>
-     
-     
-      <div className="absolute hidden md:block md:top-[400px] sm:top-[550px] sm:left-1/3 md:left-3/4  rotate-12 hover:-rotate-12 duration-1000">
-      <Snowman/>
+      <div className="absolute top-0">
+        <div className="md:mb-28 sm:mb-20 ">
+          <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+        </div>
       </div>
-      <div>
-      
+      <div className="absolute md:top-40 sm:top-32">
+        <Intro darkMode={darkMode} />
       </div>
-        <footer className='opacity-80 pb-2  mt-auto font-sans font-medium w-screen flex justify-center '>built with ❤️ anuj</footer>
-   
-    
+
+      <div className=" flex justify-center m-8">
+        <Tech />
+      </div>
+
+      <footer className="opacity-80 pb-2 pt-10 mt-auto font-sans font-medium w-screen flex justify-center ">
+        built with ❤️ anuj
+      </footer>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
